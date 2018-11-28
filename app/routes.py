44f054eafb820a6470a5289580c97609bf830e3a
Richard Hitier <richard.hitier@co-libri.org>
2018-11-28 16:07:52 +0100

    for loop in template

--- app/routes.py
+++ app/routes.py
@@ -10,3 +10,17 @@ def index():
 @app.route("/notitle")
 def notitle():
     return  render_template('index.html')
+
+@app.route("/showposts")
+def showposts():
+    posts = [
+            {
+                'author': {'username': 'Suzane'},
+                'body': 'This is my body'
+            },
+            {
+                'author': {'username': 'Richard'},
+                'body': 'See my muscles'
+            }
+            ]
+    return render_template('posts.html', posts=posts)
