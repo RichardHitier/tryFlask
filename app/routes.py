45be43e0c941c66f6705cf54caf3d4f32d577cf0
Richard Hitier <richard.hitier@co-libri.org>
2018-12-05 13:24:54 +0100

    New Register mechanism

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 from flask_login import current_user, login_user, logout_user, login_required
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegisterForm
 from app.models import User
 
 @app.route("/")
@@ -47,6 +47,21 @@ def login():
             next_page=url_for('index')
         return redirect(next_page)
     return render_template('login.html', title="Sign In", form=form)
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        #if user is None or not user.check_password(form.password.data):
+        flash('Congratulation, you are registered')
+        return redirect(url_for('index'))
+    return render_template('register.html', title="Register", form=form)
 
 @app.route("/logout")
 def logout():
