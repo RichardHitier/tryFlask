9c5278510ec9858a4597423a2a044ea24957577b
Richard Hitier <richard.hitier@co-libri.org>
2018-12-06 17:08:39 +0100

    Fix uniq name bug at profile edit

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 from models import User
 
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About Me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Change Profile')
+    # New constructor with param, called in routes.py       
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+    # Username validator, preventing allready used username
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
