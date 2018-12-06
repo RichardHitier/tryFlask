2e6b15b11d72cad9e6d3715e75b69c71a0dd5f3a
Richard Hitier <richard.hitier@co-libri.org>
2018-12-06 15:19:49 +0100

    New error handling

--- app/__init__.py
+++ app/__init__.py
@@ -13,4 +13,4 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view='login' # redirect to login route when login required
 
-from app import routes, models
+from app import routes, models, errors
