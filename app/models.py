5146519755dd00e6eb0116ed3ae8114c2e1a8898
Richard Hitier <richard.hitier@co-libri.org>
2018-12-07 12:18:54 +0100

    New followers db relationship

--- app/models.py
+++ app/models.py
@@ -7,6 +7,15 @@ from app import db
 from app import login
 
 
+# simple relation as table, not class
+#
+followers = db.Table('followers',
+        db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+        db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+        )
+
+
+
 
 # used by the flask_login extension for db interaction
 @login.user_loader
@@ -21,6 +30,12 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    # see before, the followers relationshup
+    followed = db.relationship(
+            'User', secondary=followers,
+            primaryjoin=(followers.c.follower_id == id ),
+            secondaryjoin=(followers.c.followed_id == id ),
+            backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
