128609285efa240b7b20ae8ac9b285c88f921a26
Richard Hitier <richard.hitier@co-libri.org>
2018-12-10 17:36:38 +0100

    Paginate user page

--- app/routes.py
+++ app/routes.py
@@ -26,7 +26,7 @@ def index():
         return redirect(url_for('index'))
     page_num = request.args.get('page_num', 1, type=int)
     posts = current_user.followed_posts().paginate(
-            page_num, app.config['POST_PER_PAGE'], False)
+            page_num, app.config['POSTS_PER_PAGE'], False)
     next_url = url_for('index', page_num=posts.next_num)\
             if posts.has_next else None
     prev_url = url_for('index', page_num=posts.prev_num)\
@@ -38,7 +38,7 @@ def index():
 def explore():
     page_num = request.args.get('page_num', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
-            page_num, app.config['POST_PER_PAGE'], False)
+            page_num, app.config['POSTS_PER_PAGE'], False)
     next_url = url_for('explore', page_num=posts.next_num)\
             if posts.has_next else None
     prev_url = url_for('explore', page_num=posts.prev_num)\
@@ -101,8 +101,16 @@ def logout():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = user.followed_posts()
-    return render_template('user.html', title='User Profile', user=user, posts=posts)
+    page_num = request.args.get('page_num', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+            page_num, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page_num=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page_num=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
+
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
